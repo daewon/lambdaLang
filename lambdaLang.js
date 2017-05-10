d91b01fb28e2c630afcbe74043dc6b5e4f637c0e
daewon <daewon@apache.org>
2017-05-10 20:56:48 +0900

    first commit

---
+++ lambdaLang.js
@@ -0,0 +1,58 @@
+var du = new function() {
+  this.format = function(/*args*/) {
+    var str = arguments[0];
+
+    for (var i=1; i < arguments.length; i++) {
+      str = str.replace("%s", arguments[i]);
+    }
+
+    return str;
+  };
+};
+
+function InputStream(input) {
+  return new function() {
+    var pos = 0;
+    var line = 1;
+    var col = 0;
+    var that = this;
+
+    this.next = function() {
+      var ch = input.charAt(pos);
+      pos += 1;
+
+      if (ch == "\n") {
+        line += 1;
+        col = 0 ;
+      } else {
+        col += 1;
+      }
+
+      return ch;
+    };
+
+    this.peek = function() {
+      return input.charAt(pos);
+    };
+
+    this.eof = function() {
+      return that.peek() == "";
+    };
+
+    this.fail = function(msg) {
+      throw new Error(du.format("%s (%s:%s)", msg, line, col));
+    };
+
+    this.debug = function() {
+      console.log(du.format("line: %s, col: %s, pos: %s", line, col, pos));
+    };
+  };
+}
+
+
+var stream = InputStream("daewon\njeong\nlang");
+
+while(stream.peek()) {
+  var ch = stream.next();
+  stream.debug();
+}
