5d74257500ce4bcce32f52b0f517b32ab4723617
daewon <daewon@apache.org>
2017-09-20 14:36:07 +0900

    add first test

--- src/lexer.js
+++ src/lexer.js
@@ -1,85 +1,182 @@
-exports.InputStream = InputStream
-exports.TokenStream = TokenStream
+class InputStream {
+  constructor(input) {
+    this.input = input;
 
-function InputStream(input) {
-  return new function() {
-    let _pos = 0;
-    let _line = 1;
-    let _col = 0;
+    this.pos = 0;
+    this.line = 1;
+    this.col = 0;
+  }
 
-    // EXPOSE
-    return new function() {
-      this.next = next;
-      this.peek = peek;
-      this.eof = eof;
-      this.fail = fail;
-      this.debug = debug;
-    };
+  next() {
+    const ch = this.input.charAt(this.pos++);
+    if (ch == "\n") this.line++, this.col = 0; else this.col++;
 
-    /** private  **/
-    function next() {
-      let ch = input.charAt(_pos);
-      _pos += 1;
+    return ch;
+  }
 
-      if (ch == "\n") {
-        _line += 1;
-        _col = 0 ;
-      } else {
-        _col += 1;
+  peek() {
+    return this.input.charAt(this.pos);
+  }
+
+  eof() {
+    return this.peek() == "";
+  }
+
+  croak(msg) {
+    throw new Error(msg + " (" + this.line + ":" + this.col + ")");
+  }
+}
+
+class TokenStream {
+  constructor(input) {
+    this.input = input;
+
+    this.current = null;
+    this.KEYWORDS = ["if", "then", "else",  "lambda",  "λ",  "true",  "false"];
+  }
+
+  is_keyword(x) {
+    return this.KEYWORDS.includes(x);
+  }
+
+  is_digit(ch) {
+    return /[0-9]/i.test(ch);
+  }
+
+  is_id_start(ch) {
+    return /[a-zλ_]/i.test(ch);
+  }
+
+  is_id(ch) {
+    return this.is_id_start(ch) || "?!-<>=0123456789".includes(ch);
+  }
+
+  is_op_char(ch) {
+    return "+-*/%=&|<>!".includes(ch);
+  }
+
+  is_punc(ch) {
+    return ",;(){}[]".includes(ch);
+  }
+
+  is_whitespace(ch) {
+    return " \t\n".includes(ch);
+  }
+
+  read_while(predicate) {
+    const arr = [];
+    while (!this.input.eof() && predicate(this.input.peek())) {
+      arr.push(this.input.next());
+    }
+
+    return arr.join("");
+  }
+
+  read_number() {
+    let has_dot = false;
+    let number = this.read_while(ch => {
+      if (ch == ".") {
+        if (has_dot) { return false; }
+        has_dot = true;
+
+        return true;
       }
 
-      return ch;
-    };
+      return this.is_digit(ch);
+    });
 
-    function peek() {
-      return input.charAt(_pos);
-    };
+    return { type: "num", value: parseFloat(number) };
+  }
 
-    function eof() {
-      return self.peek() == "";
-    };
+  read_ident() {
+    const id = this.read_while(this.is_id.bind(this));
 
-    function fail(msg) {
-      throw new Error(`${msg} (${_line}:${_col}`);
+    return {
+      type  : this.is_keyword(id) ? "kw" : "var",
+      value : id
     };
+  }
 
-    function debug() {
-      console.log(`line: ${_line}, col: ${_col}, pos: ${_pos}`);
-    };
+  read_escaped(end) {
+    let escaped = false, arr = [];
+    this.input.next();
+
+    while (!this.input.eof()) {
+      const ch = this.input.next();
+      if (escaped) {
+        arr.push(ch);
+        escaped = false;
+      } else if (ch == "\\") {
+        escaped = true;
+      } else if (ch == end) {
+        break;
+      } else {
+        arr.push(ch);
+      }
+    }
+
+    return arr.join("");
+  }
+
+  read_string() {
+    return { type: "str", value: this.read_escaped('"') };
+  }
+
+  skip_comment() {
+    this.read_while(ch => { return ch != "\n"; });
+    this.input.next();
+  }
+
+  read_next() {
+    this.read_while(this.is_whitespace);
+    if (this.input.eof()) { return null; };
+
+    const ch = this.input.peek();
+
+    if (ch == "#") {
+      this.skip_comment();
+      return this.read_next();
+    }
+
+    if (ch == '"') { return this.read_string(); }
+    if (this.is_digit(ch)) { return this.read_number(); }
+    if (this.is_id_start(ch)) { return this.read_ident(); }
+    if (this.is_punc(ch)) {
+      return {
+        type  : "punc",
+        value : this.input.next()
+      };
+    }
+
+    if (this.is_op_char(ch)) {
+      return {
+        type  : "op",
+        value : this.read_while(this.is_op_char)
+      };
+    }
+
+    this.input.croak("Can't handle character: " + ch);
   };
+
+  peek() {
+    if (!this.current) {
+      this.current = this.read_next();
+    }
+
+    return this.current;
+  }
+
+  next() {
+    const tok = this.current;
+    this.current = null;
+
+    return tok || this.read_next();
+  }
+
+  eof() {
+    return this.peek() == null;
+  }
 }
 
-function TokenStream(inputStream) {
-  let KEYWORDS = ["if", "then", "else", "lambda", "true", "false"];
-
-  var _status= {
-    current: null
-  };
-
-  /**
-     public apis
-  **/
-  return {
-    next: next,
-    peek: peek,
-    eof: eof,
-    error: inputStream.error
-  };
-
-  function next() {
-  };
-
-  function peek() {
-  };
-
-  function eof() {
-  };
-
-  function punc(v) { return { type: "punc", value: v }; } ;
-  function num(n) { return { type: "num", value: n };  };
-  function str(str) { return { type: "str", value: str }; }
-  function keyword(kwd) { return { type: "keyword", value: kwd  }; }
-  function variable(variable) { return { type: "variable", value: variable }; }
-  function op(op) { return { type: "op", value: op }; }
-}
-
+exports.InputStream = InputStream;
+exports.TokenStream = TokenStream;
--- test/test.js
+++ test/test.js
@@ -1,15 +1,28 @@
-const assert = require('assert')
-const lexer = require('../src/lexer');
+const assert = require("assert");
+const lexer = require("../src/lexer");
 
-const Is = lexer.InputStream
-const Ts = lexer.TokenStream
+const Is = lexer.InputStream;
+const Ts = lexer.TokenStream;
 
-describe('Lexer', () => {
-  describe('InputStream', () => {
+describe("Lexer", () => {
+  describe("InputStream", () => {
+    const str = "abcdefg{}[] ";
+    const is = new Is(str);
 
-    it('should return -1', () => {
-      assert.equal(-1, -1)
-    })
+    it("next", () => {
+      str.split("").forEach(ch => {
+        assert.equal(is.peek(), ch);
+        assert.equal(is.next(), ch); // advance
+      });
 
+      assert.equal(is.eof(), true);
+    });
+  });
+
+  describe("TokenStream", () => {
+    it("identifier", () => {
+      const ts = new Ts(new Is("if"));
+      assert.deepEqual(ts.peek(), { type: "kw", value: "if" });
+    });
   });
 });
